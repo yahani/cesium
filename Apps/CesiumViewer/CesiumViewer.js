49b60616173412342e0ff966420680ac566bbb8e
Matthew Amato <matt.amato@gmail.com>
2012-11-28 13:24:38 -0500

    Fixes after integrate.

--- Apps/CesiumViewer/CesiumViewer.js
+++ Apps/CesiumViewer/CesiumViewer.js
@@ -7,7 +7,7 @@ define([
     'Widgets/Dojo/CesiumViewerWidget',
     'Core/Matrix4',
     'Core/Cartesian3',
-    'Scene/Model',
+    'Scene/Model'
 
 ], function(
     dom,
@@ -36,32 +36,32 @@ define([
 
         widget.startup();
 
-        var scene = widget.scene;
-//        scene.getContext().setValidateShaderProgram(true);
-//        scene.getContext().setValidateFramebuffer(true);
-//        scene.getContext().setLogShaderCompilation(true);
-//        scene.getContext().setThrowOnWebGLError(true);
-
-        var primitives = scene.getPrimitives();
-
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_Internet Man 2/Internet Man.json'); // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_A320-200/models/untitled.json');    // OK.  Needs nodes combined.
-
-        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_FA-18_Hornet/model0.json');         // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_M1A2_Abrams/model0.json');          // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_M1043_HMMWV/model0.json');          // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_C-130-Hercules/model0.json');       // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_Ford_Contour_Sedan/model0.json');   // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_RQ-1_Predator/model0.json');        // OK
-
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/duck/duck.json');                     // OK
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/rambler/Rambler.json');               // Can't invert matrix
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/SuperMurdoch/SuperMurdoch.json');     // OK. scale = 9000.0.
-//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/wine/wine.json');                     // OK.
-
-        m.scale = 900000.0;
-//        m.modelMatrix = Matrix4.fromTranslation(new Cartesian3(8000000.0, 0.0, 0.0));
-        primitives.add(m);
-        scene.getPrimitives().setCentralBody(undefined);
+//        var scene = widget.scene;
+////        scene.getContext().setValidateShaderProgram(true);
+////        scene.getContext().setValidateFramebuffer(true);
+////        scene.getContext().setLogShaderCompilation(true);
+////        scene.getContext().setThrowOnWebGLError(true);
+//
+//        var primitives = scene.getPrimitives();
+//
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_Internet Man 2/Internet Man.json'); // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_A320-200/models/untitled.json');    // OK.  Needs nodes combined.
+//
+//        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_FA-18_Hornet/model0.json');         // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_M1A2_Abrams/model0.json');          // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_M1043_HMMWV/model0.json');          // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_C-130-Hercules/model0.json');       // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_Ford_Contour_Sedan/model0.json');   // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/_RQ-1_Predator/model0.json');        // OK
+//
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/duck/duck.json');                     // OK
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/rambler/Rambler.json');               // Can't invert matrix
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/SuperMurdoch/SuperMurdoch.json');     // OK. scale = 9000.0.
+////        var m = new Model('../../../Apps/CesiumViewer/Gallery/Models/wine/wine.json');                     // OK.
+//
+//        m.scale = 900000.0;
+////        m.modelMatrix = Matrix4.fromTranslation(new Cartesian3(8000000.0, 0.0, 0.0));
+//        primitives.add(m);
+//        scene.getPrimitives().setCentralBody(undefined);
     });
 });
